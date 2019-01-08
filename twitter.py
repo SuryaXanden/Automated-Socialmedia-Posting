3d1c446447e8031102db19c14847996977cca039
Surya Xanden <suryan1997@gmail.com>
2019-01-09 01:56:11 +0530

    Graceful degradation

--- twitter.py
+++ twitter.py
@@ -10,24 +10,43 @@ import shadow
 # 2 for fast
 
 def Twitter(usr,pwd,path,desc,speed):
+    #browser
     driver = webdriver.Chrome(executable_path='./chromedriver.exe')
+    #URL
     driver.get('https://twitter.com/login')
+    #USERNAME
     usr_box = driver.find_element_by_class_name('js-username-field')
+    #sending username
     usr_box.send_keys(usr)
+    #PASSWORD
     pwd_box = driver.find_element_by_class_name('js-password-field')
+    #sending password
     pwd_box.send_keys(pwd)
-    sleep(speed)
+    #LOGIN
     login_button = driver.find_element_by_css_selector('button.submit.EdgeButton.EdgeButton--primary.EdgeButtom--medium')
+    #clicking on login
     login_button.submit()
+
+    #Wait until login
     sleep(speed)
+
+    #ATTACH MEDIA
     image_box = driver.find_element_by_css_selector('input.file-input.js-tooltip')
+    #sending media
     image_box.send_keys(path)
+
+    #wait for a bit (until its uploaded to browser maybe )
     sleep(speed)
+
+    #DESCRIPTION
     text_box = driver.find_element_by_id('tweet-box-home-timeline')
+    #sending description
     text_box.send_keys(desc)
-    sleep(speed)
+    #TWEET
     tweet_button = driver.find_element_by_css_selector('button.tweet-action.EdgeButton.EdgeButton--primary.js-tweet-btn')
     tweet_button.click()
+    sleep(speed*1.5)
+    driver.close()
     return
 
 app = Flask(__name__)
