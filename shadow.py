92e0c514e5a2fa349cf35bbedefae2667bcdf67b
Surya Xanden <suryan1997@gmail.com>
2019-01-09 15:04:48 +0530

    Add files via upload

---
+++ shadow.py
@@ -0,0 +1,11 @@
+
+Tusr = 'suryaxanden'
+##
+Tpwd = 'SURYAXANDEN123TWITTWEISFREESX'
+##
+Fusr = 'raiden4surya@gmail.com'
+##
+Fpwd = 'SXSURYAXANDENDONDEMANDOXS'
+##
+Iusr = Tusr
+Ipwd = 'SURYAXANDENKINGOFDONDEMANDOSX'
